0664a6b9f2743e14d1b77ea48e1dcc01558785a5
Tharindu Hewawitharana <thasanhe1990@gmail.com>
2020-05-23 19:48:50 +0400

    Refactoring

--- lib/widgets/final_result.dart
+++ lib/widgets/final_result.dart
@@ -9,18 +9,22 @@ class FinalResult extends StatelessWidget{
 
   FinalResult(this._answers, this._resetQuiz, this._score);
 
+  String get resultText {
+    return "You have done it!";
+  }
+
   @override
   Widget build(BuildContext context) {
       return Container(
           width: double.infinity,
           child: Column(
             children : [
-              Container(child : Text("You have done it!", style: TextStyle(fontSize: 23, color: Colors.white), textAlign: TextAlign.center,), width: double.infinity, color: Colors.green,),
+              Container(child : Text(resultText, style: TextStyle(fontSize: 23, color: Colors.white), textAlign: TextAlign.center,), width: double.infinity, color: Colors.green,margin: EdgeInsets.all(5),),
               ..._answers.map((selection) => Container(
                 child : Text(selection, textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
                 padding: EdgeInsets.all(5),
-                margin: EdgeInsets.only(top: 5),color: Colors.blue, width: double.infinity,)).toList(),
-              Text("Your score is : " + _score.toString()),
+                margin: EdgeInsets.only(top: 5, left: 5, right: 5),color: Colors.blue, width: double.infinity,),).toList(),
+              Text("Your score is : " + _score.toString(),),
               RaisedButton(
                 child: Text("Restart quiz"),
                 onPressed: _resetQuiz,
